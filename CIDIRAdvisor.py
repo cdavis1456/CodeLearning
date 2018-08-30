3ec837fbcd8d45209e77450d29f8cab6bbe18920
Christopher Davis <31138928+cdavis1456@users.noreply.github.com>
2018-08-30 11:19:39 -0500

    Add files via upload

---
+++ CIDIRAdvisor.py
@@ -0,0 +1,11 @@
+#Chris CDIR Advisor !!
+
+print('How many available addresses do you need?')
+m = input(x)
+if 0 < m < 2 ** 23
+    for i in reversed(range(33))
+        ipavailable=  ((31- i)**2) + 1
+        if ipavailable is >= m
+            print (i)
+            break
+else print ('Failed Response')
